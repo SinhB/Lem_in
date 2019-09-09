619c806cf43ba0551b0f49bb136f0d1965540755
yannbecret <ybecret@student.42.fr>
2019-09-09 11:50:44 +0200

    sort paths done

--- srcs/paths.c
+++ srcs/paths.c
@@ -6,7 +6,7 @@
 /*   By: mjouffro <mjouffro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 16:26:52 by mjouffro          #+#    #+#             */
-/*   Updated: 2019/09/04 17:16:49 by yabecret         ###   ########.fr       */
+/*   Updated: 2019/09/09 11:48:48 by yabecret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,52 @@ int			get_matrix_path(t_lemin *lemin, t_links *tmp)
 	return (len);
 }
 
+t_allpaths *sort_paths(t_allpaths *container)
+{
+	t_allpaths *swap;
+	t_allpaths *swap2;
+	t_allpaths *pred;
+	t_allpaths *head;
+	int i = 0;
+
+	head = container;
+	pred = head;
+	while (container->next)
+	{
+//		ft_printf("pred | %d |\n", pred->len);
+//		ft_printf("%d | %d\n", container->len, container->next->len);
+		if (container->len > container->next->len)
+		{
+			swap = container;
+//			ft_printf("swap len is : %d\n", swap->len);
+//			ft_printf("Before container len is : %d\n", container->len);
+			container = container->next;
+			pred->next = container;
+			swap2 = container->next;
+			swap->next = swap2;
+//			ft_printf("After  container len is : %d\n", container->len);
+			container->next = swap;
+			if (swap == head)
+				head = container;
+			swap = NULL;
+//			ft_printf("Container next len is : %d\n", container->next->len);
+			container = head;
+			pred = head;
+//			ft_printf("{red}BACK{reset}\n");
+			i++;
+		}
+		else
+		{
+//			ft_printf("NEXT\n");
+			if (container != head)
+				pred = pred->next;
+			container = container->next;
+		}
+	}
+	container = head;
+	return (container);
+}
+
 void		get_final_paths(t_lemin *lemin)
 {
 	t_links 	*tmp;
@@ -79,7 +125,7 @@ void		get_final_paths(t_lemin *lemin)
 	t_allpaths	*head;
 	int 		length;
 	int			i;
-	int			nb_steps;
+//	int			nb_steps;
 
 	lemin->nb_paths = find_nb_paths(lemin);
 	tmp = memalloc_links();
@@ -96,7 +142,7 @@ void		get_final_paths(t_lemin *lemin)
 		lemin->container1->len = length;
 		ft_printf("{yellow} final solution : {reset}\n");
 		print(lemin->container1->path);
-		//ft_printf("path len is : %d\n", lemin->container->len);
+		ft_printf("path len is : %d\n", lemin->container1->len);
 //		printmatrix(lemin->matrix, lemin->cnt);
 		if (i != lemin->nb_paths - 1)
 		{
@@ -107,6 +153,16 @@ void		get_final_paths(t_lemin *lemin)
 		i++;
 	}
 	lemin->container1 = head;
-	nb_steps = nbr_steps(lemin, head);
-	ft_printf("FINAL nbr_steps is %d\n", nb_steps);
+	ft_printf("{yellow} AFTER SORT : {reset}\n");
+	lemin->container1 = sort_paths(lemin->container1);
+	while (lemin->container1)
+	{
+		ft_printf("{yellow} final solution : {reset}\n");
+		print(lemin->container1->path);
+		ft_printf("path len is : %d\n", lemin->container1->len);
+		lemin->container1 = lemin->container1->next;
+	}
+	//	lemin->container1 = head;
+	//	nb_steps = nbr_steps(lemin, head);
+	//	ft_printf("FINAL nbr_steps is %d\n", nb_steps);
 }
